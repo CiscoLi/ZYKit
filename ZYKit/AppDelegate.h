dc3a2c302316127c9defe3b6a232db2b3cfc6273
lizhaoyang <ciscolee@126.com>
2016-11-30 15:48:08 +0800

    初始化项目

---
+++ ZYKit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZYKit
+//
+//  Created by zhaoyang on 2016/11/30.
+//  Copyright © 2016年 zhaoyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZYKit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZYKit
+//
+//  Created by zhaoyang on 2016/11/30.
+//  Copyright © 2016年 zhaoyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
